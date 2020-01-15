be7f2b403115c91195efb58065fbf6037b95168e
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2020-01-15 11:15:05 +0100

    sohigh heeft de laatste aanpassingen gedaan aan de code

---
+++ _ster.js
@@ -0,0 +1,34 @@
+class Ster {
+    __construct(afbNaam, i) {
+        this.x = 300 + 20*i;
+        this.y = 300 + 10*i;
+        this.afbNaam = 'afb/' + afbNaam;
+        var nweAfb = document.createElement('img');
+        nweAfb.src = this.afbNaam;
+        nweAfb.className = 'volg';
+        nweAfb.style.left = this.x + 'px';
+        nweAfb.style.top = this.y + 'px';
+        nweAfb.style.webkitTransform = 'rotate('+7*i+'deg)';
+        document.getElementById('volgers').appendChild(nweAfb);
+        //de beweging
+        this.vx = 0;
+        this.vy = 0;
+        this.draaiing = 7*i;
+        
+        this.bewegen = function (invX, invY) {
+            this.vx += (invX-this.x)*this.veerconstante;
+            this.vY += (invY-this.Y)*this.veerconstante;
+            this.vx *= (invX-this.x)*this.veerconstante;
+            this.vx *= this.demping;
+            this.vY += this.demping;
+            this.x += this.vx;
+            this.y += this.vy;
+            this.draaiing += this.vx;
+            nweAfb.style.left = this.x - this.vedfschuiving + 'px';
+            nweAfb.style.webkitTransform = 'rotate('+this.draaiing+'deg)';
+        
+    }
+    Ster.prototype.verschuiving = 25;
+    Ster.prototype.veerconstante = 0.1;
+    Ster.prototype.demping = 0.8;
+}}
--- muistracker.js
+++ muistracker.js
@@ -1,11 +1,15 @@
 var textstXp = document.getElementById('textX');
 var textstYp = document.getElementById('textY');
-var deAfbeelding = ["star.png", "ster.png", "sterr.png"];
+var deAfbeeldingen = ["star.png", "ster.png", "sterr.png"];
 
+var alleVolgers = [];
+var muisX = 0;
+var muisY = 0;
 
-for(i=0; i<deAfbeelding.length; i++) {
-    var naam = deAfbeelding[i];
+for(i=0; i<deAfbeeldingen.length; i++) {
+    var naam = deAfbeeldingen[i];
     var ster = new Ster(naam, i);
+    alleVolgers.push(ster);
 }
 
 (function doeDitSteeds () {
@@ -13,6 +17,19 @@ for(i=0; i<deAfbeelding.length; i++) {
     function geefMuisPositie(e) {
         textstXp.innerHTML = e.clientX;
         textstYp.innerHTML = e.clientY;
+        muisX = e.clientX;
+        muisY = e.clientY;
+    }
+
+    for(var i=0; i<alleVolgers.length; i++) {
+        var volger = alleVolgers[i];
+        var doelX = muisX;
+        var doelY = muisY;
+        if(i>0) {
+            doelX = alleVolgers[i-1].x;
+            doelY = alleVolgers[i-1].y;
+        };
+        volger.bewegen(doelX,doelY);
     }
     requestAnimationFrame(doeDitSteeds);
 }());
